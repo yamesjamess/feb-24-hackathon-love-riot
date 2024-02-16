2564782ffb528529d68dd899c95bee37a27f0076
ilyasolgun11 <127804888+ilyasolgun11@users.noreply.github.com>
2024-02-16 18:56:35 +0000

    Change style of homepage, add logic to js to increment percentage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,43 @@ const compatibilityScoreForm = document.querySelector('.user-input')
 const compatibilityPercentage = document.getElementById('compatibility-percentage')
 const resetForm = document.querySelector('.fa-rotate-left')
 const userAndCrushInputs = document.querySelectorAll('.input')
+const heartImg = document.querySelector('.compatibility-section-img')
+const headerH1 = document.querySelector('.header-h1')
+const loadingSpinner = document.querySelector('.loading-spinner')
 
+// Dummy user compatibility score 
+const compatibilityScore = 89;
+let initialScore = compatibilityScore - 30;
 compatibilityScoreForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    compatibilityScoreContainer.style.visibility = "visible";
-    compatibilityScoreContainer.style.opacity = "1";
-})
+    e.preventDefault();
+    heartImg.style.visibility = "collapse";
+    headerH1.textContent = "Crunching numbers, igniting sparks!";
+    loadingSpinner.style.visibility = "visible";
+    loadingSpinner.style.opacity = "1";
+    setTimeout(() => {
+        const intervalId = setInterval(() => {
+            initialScore += 1;
+            if (initialScore > compatibilityScore) {
+                initialScore = compatibilityScore;
+                clearInterval(intervalId);
+            } else {
+                compatibilityPercentage.textContent = initialScore + "%";
+            }}, 20);
+                headerH1.textContent = "Love Riot Calculator";
+                compatibilityScoreContainer.style.visibility = "visible";
+                compatibilityScoreContainer.style.opacity = "1";
+                loadingSpinner.style.visibility = "collapse";
+                loadingSpinner.style.opacity = "0";
+            }, 1500);
+        
+});
 
 userAndCrushInputs.forEach((input) => {
     resetForm.addEventListener('click', () => {
         input.value = ''; 
         compatibilityScoreContainer.style.visibility = "collapse";
         compatibilityScoreContainer.style.opacity = "0";
-        console.log("Removed");
+        heartImg.style.visibility = "visible"
     })
     
 });
